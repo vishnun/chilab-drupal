62e43672239469818f2c1eda00bf780f8f4ab6c5
Vishnu <vishnunarang2000@gmail.com>
2017-04-17 11:48:41 -0700

    Setting the people carousel to auto scroll.

--- themes/chilab/js/people.js
+++ themes/chilab/js/people.js
@@ -1,5 +1,8 @@
 $ = jQuery;
 $(document).ready(function () {
     $('.parallax').parallax();
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel.carousel-slider').carousel({fullWidth: true, duration: 500});
+    setInterval(function () {
+        $('.carousel').carousel('next');
+    }, 5000);
 });
